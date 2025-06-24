07887ee05f6b27da5948b95cb3dc1275f0ad6759
d-ito1106 <d_ito@taiho-pp.co.jp>
2025-06-24 12:47:59 +0900

    first commit

--- app/src/main/java/com/example/jettodo/MainActivity.kt
+++ app/src/main/java/com/example/jettodo/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.jettodoapp
+package com.example.jettodo
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -10,24 +10,34 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.jettodoapp.ui.theme.JetTodoAppTheme
-import dagger.hilt.android.AndroidEntryPoint
+import com.example.jettodo.ui.theme.JetTodoTheme
 
-@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetTodoAppTheme {
+            JetTodoTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-
+                    Greeting("Android")
                 }
             }
         }
     }
 }
 
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JetTodoTheme {
+        Greeting("Android")
+    }
+}
--- app/src/test/java/com/example/jettodo/ExampleUnitTest.kt
+++ app/src/test/java/com/example/jettodo/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.jettodoapp
+package com.example.jettodo
 
 import org.junit.Test
 
