35021daee5af73a35b66674bf7921559167556bb
d-ito1106 <d_ito@taiho-pp.co.jp>
2025-06-24 15:22:01 +0900

    37. TaskDaoインターフェースを新規作成し、基本的なCRUD操作を定義

---
+++ app/src/main/java/com/example/jettodo/TaskDao.kt
@@ -0,0 +1,23 @@
+package com.example.jettodo
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface TaskDao {
+
+    @Insert
+    fun insertTask(task: Task)
+
+    @Query("SELECT * FROM Task")
+    fun loadAllTasks(): List<Task>
+
+    @Update
+    fun updateTask(task: Task)
+
+    @Delete
+    fun deleteTask(task: Task)  
+}
