b2bf721695a352fac9fc7fe6b4609a8098b32e3f
d-ito1106 <d_ito@taiho-pp.co.jp>
2025-07-03 09:48:42 +0900

    39

--- app/src/main/java/com/example/jettodo/AppDatabase.kt
+++ app/src/main/java/com/example/jettodo/AppDatabase.kt
@@ -1,9 +1,10 @@
 package com.example.jettodo
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 
 @Database(entities = [Task::class], version = 1)
-abstract class AppDatabase {
+abstract class AppDatabase :RoomDatabase(){
     abstract fun taskDao(): TaskDao
 
 }
