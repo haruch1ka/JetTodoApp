0393fcd2c76cc367a28a0b96781cb128d50cb253
d-ito1106 <d_ito@taiho-pp.co.jp>
2025-07-03 10:59:57 +0900

    デプロイメントターゲットの設定を修正し、MainActivityに新しいコンテンツを追加

--- app/src/main/java/com/example/jettodo/MainActivity.kt
+++ app/src/main/java/com/example/jettodo/MainActivity.kt
@@ -3,10 +3,11 @@ package com.example.jettodo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -31,3 +32,15 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Composable
+fun MainContent(){
+    Scaffold(floatingActionButton = {
+        FloatingActionButton(onClick = { /*TODO*/ }) {
+            Icon(imageVector = Icons.Default.Add, contentDescription = "Add Task" )
+        }
+    }) {
+        // Main content goes here
+        Text(text = "Hello, Jetpack Compose!")
+
+    }
+}
